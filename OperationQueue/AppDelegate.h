ed65360956173f714e148848b7f739a3b273aa81
Gopal Vaid <gopalvaid@ravis-mac-mini.local>
2016-04-18 11:54:10 +0530

    Initial Commit

---
+++ OperationQueue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OperationQueue
+//
+//  Created by Gopal.Vaid on 18/04/16.
+//  Copyright © 2016 Gopal.Vaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
